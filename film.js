3b9e6e6c728e6ab7ac22b2783f855551b24dda6d
onurtalayhan <onur.talayhan@gmail.com>
2020-12-12 22:30:07 +0100

    js files modified/pc1

--- film.js
+++ film.js
@@ -1,12 +1,6 @@
-/* //Film Constructor
 function Film(title,director,url){
     this.title = title;
     this.director = director;
     this.url = url;
-} */
 
-function Film(title,director,url){
-    this.title = title;
-    this.director = director;
-    this.url = url;
 }
--- project.js
+++ project.js
@@ -2,30 +2,78 @@ const form = document.getElementById("film-form");
 const titleElement = document.querySelector("#title");
 const directorElement = document.querySelector("#director");
 const urlElement = document.querySelector("#url");
+const cardbody = document.querySelectorAll(".card-body")[1];
+const clear = document.getElementById("clear-films");
 
 
-//start UI
+
 const ui = new UI();
 
+
+const storage = new Storage();
+
+
+
 eventListeners();
+
 function eventListeners(){
     form.addEventListener("submit",addFilm);
-    }
+    document.addEventListener("DOMContentLoaded",function(){
+        let films = storage.getFilmsFromStorage();
+        ui.loadAllFilms(films);
+
+    });
+
+    cardbody.addEventListener("click",deleteFilm);
+    clear.addEventListener("click",clearAllFilms);
+
+}
 function addFilm(e){
-    
     const title = titleElement.value;
     const director = directorElement.value;
     const url = urlElement.value;
-    console.log(title, director, url)
-if (title === "" || director === "" || url === ""){
+
+    if (title === "" || director === "" || url === ""){
+        
+        ui.displayMessages("Fill in all fields ...","danger");
+
+    }
+    else {
+        
+        const newFilm = new Film(title,director,url);
+
+        ui.addFilmToUI(newFilm); 
+        storage.addFilmToStorage(newFilm); 
+
+        ui.displayMessages("The movie was successfully added ...","success");
+
+
+    }
+
+
+    ui.clearInputs(titleElement,urlElement,directorElement);
+
+    e.preventDefault();
+}
+
+function deleteFilm(e){
+
+    if (e.target.id === "delete-film") {
+        ui.deleteFilmFromUI(e.target);
+        storage.deleteFilmFromStorage(e.target.parentElement.previousElementSibling.previousElementSibling.textContent);
+
+        ui.displayMessages("Deletion is successful ..","success");
+
+    }
 
 }
-else{
+function clearAllFilms(){
 
-    const newFilm = new Film (title, director,url)
-    ui.addFilmToUI(newFilm);
+    if (confirm("Are you sure ?")) {
+        ui.clearAllFilmsFromUI();
+        storage.clearAllFilmsFromStorage();
+
+    }
    
-}
 
-    e.preventDefault;
 }
